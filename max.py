22929ddb49a54b9bfbd3348272a68c6ac6bc1afc
Jamey Gronewald <jrgronewald@gmail.com>
2020-10-02 10:23:56 -0700

    find_max simplified

--- max.py
+++ max.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
     
-    def find_max(*args):
+    def find_max(*nums):
         largest = None
-        num_list = [num for num in args]
-        for num in num_list:
+        # num_list = [num for num in nums]
+        # for num in num_list:
+        for num in nums:
             if largest is None:
                 largest = num
             elif largest < num:
